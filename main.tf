78ed81b5ff8ce4896abb8dbf7fcf2b1da08004aa
Liubov Smahliuk <liubovsmahliuk@Liubovs-MacBook-Pro.local>
2024-06-27 14:12:56 +0300

    testing

--- main.tf
+++ main.tf
@@ -2,8 +2,14 @@ provider "aws" {
     region = "us-east-1"
 }
 
-# resource "aws_s3_bucket" "this" {}
+resource "aws_s3_bucket" "this" {}
 
-# resource "aws_s3_bucket" "already_exeisting_bucket" {
+resource "aws_s3_bucket" "already_exeisting_bucket" {
+    bucket = "terraform-state-aekoow9loo7voh4on5p"
+}
 
-# }
+resource "aws_ssm_parameter" "dependant_resource" {
+    name = "/dependant/bucket_id"
+    value = aws_s3_bucket.already_exeisting_bucket.id
+    type = "String"
+}
