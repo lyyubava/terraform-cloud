5cb5762d3e2d1ad9b94edacc8a2bcc80d6f6e413
Liubov Smahliuk <liubovsmahliuk@Liubovs-MacBook-Pro.local>
2024-06-27 11:47:14 +0300

    ci

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ provider "aws" {
     region = "us-east-1"
 }
 
-# resource "aws_s3_bucket" "this" {}
+resource "aws_s3_bucket" "this" {}
+
+# resource "aws_s3_bucket" "already_exeisting_bucket" {
+
+# }
