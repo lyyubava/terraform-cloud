1f1971b499abadd8b3d194d1cffb45cfeddd290e
Liubov Smahliuk <liubov.smahliuk@otakoyi.com>
2024-07-01 16:27:40 +0300

    add sleep

--- main.tf
+++ main.tf
@@ -6,8 +6,16 @@ resource "aws_s3_bucket" "this" {}
 
 resource "aws_s3_bucket" "already_existing_bucket" {
     bucket = "terraform-state-aekoow9loo7voh4on5p"
+    depends_on = [ null_resource.sleep ]
 }
 
+resource "null_resource" "sleep" {
+  provisioner "local-exec" {
+    command     = "sleep 600"
+    interpreter = ["bash", "-c"]
+  }
+
+}
 # resource "aws_ssm_parameter" "dependant_resource" {
 #     name = "/dependant/bucket_id"
 #     value = aws_s3_bucket.already_exeisting_bucket.id
