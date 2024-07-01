886cdd340492d5dd942fbc169cf6f12477f9b630
Liubov Smahliuk <liubov.smahliuk@otakoyi.com>
2024-07-01 15:06:50 +0300

    testnew

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "aws_s3_bucket" "already_existing_bucket" {
     bucket = "terraform-state-aekoow9loo7voh4on5p"
 }
 
-resource "aws_ssm_parameter" "dependant_resource" {
-    name = "/dependant/bucket_id"
-    value = aws_s3_bucket.already_exeisting_bucket.id
-    type = "String"
-}
+# resource "aws_ssm_parameter" "dependant_resource" {
+#     name = "/dependant/bucket_id"
+#     value = aws_s3_bucket.already_exeisting_bucket.id
+#     type = "String"
+# }
