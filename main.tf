f01547ac9e69707093d3ad5fd2d791c863fd4fd6
Liubov Smahliuk <liubov.smahliuk@otakoyi.com>
2024-07-03 17:44:09 +0300

    testtimeout

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_s3_bucket" "this" {
       Name        = "My bucket"
       Environment = "prod"
     }
+
 }
 resource "aws_s3_bucket" "this1" {
     tags = {
@@ -15,22 +16,33 @@ resource "aws_s3_bucket" "this1" {
     }
 }
 
+
+resource "aws_s3_bucket" "this2" {
+    tags = {
+      Name        = "My this1"
+      Environment = "prod"
+    }
+    timeouts {
+      create = "2s"
+    }
+    depends_on = [ null_resource.sleep ]
+}
 resource "aws_s3_bucket" "already_existing_bucket" {
     bucket = "terraform-state-aekoow9loo7voh4on5p"
     tags = {
       Name        = "My bucket"
     }
-    depends_on = [ aws_s3_bucket.this, aws_s3_bucket.this1  ]
+    depends_on = [ aws_s3_bucket.this, aws_s3_bucket.this1, aws_s3_bucket.this2 ]
     # depends_on = [ null_resource.sleep ]
 }
 
-# resource "null_resource" "sleep" {
-#   provisioner "local-exec" {
-#     command     = "sleep 600"
-#     interpreter = ["bash", "-c"]
-#   }
+resource "null_resource" "sleep" {
+  provisioner "local-exec" {
+    command     = "sleep 60"
+    interpreter = ["bash", "-c"]
 
-# }
+  }
+}
 # resource "aws_ssm_parameter" "dependant_resource" {
 #     name = "/dependant/bucket_id"
 #     value = aws_s3_bucket.already_exeisting_bucket.id
