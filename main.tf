29f297e51b7e58ac18bd5981a7568b8eba8b448f
Liubov Smahliuk <liubov.smahliuk-contractor@procore.com>
2024-07-30 02:59:15 +0300

    test

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "this3" {
       Environment = "prod"
       KK = "ss"
       "pcor:system" = "test-system"
-      # "pcor:domain" = "test-domain"
+      "pcor:domain" = "test-domain1"
     }
     timeouts {
       create = "1s"
