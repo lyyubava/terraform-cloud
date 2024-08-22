4ab79afde0f82f5baa1fce7024cf7c9fe9df9cd0
Liubov Smahliuk <liubov.smahliuk-contractor@procore.com>
2024-08-22 16:01:00 +0300

    test-run

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      "pcor-system" = "equipment"
+      "pcor-domain" = "cloud-runtime"
+    }
+  }
 }
 
 resource "aws_instance" "my_instance" {
@@ -14,21 +20,3 @@ resource "aws_instance" "my_instance" {
   }
 }
 
-data "aws_instances" "filtered_instance" {
-  filter {
-    name   = "tag:pcor-system"
-    values = ["invalid-system"]
-  }
-  filter {
-    name   = "tag:pcor-domain"
-    values = ["invalid-domain"]
-  }
-}
-
-  # default_tags {
-  #   tags = {
-  #     "pcor-system" = "Test"
-  #     "pcor-system" = "equipment"
-  #     "pcor-domain" = "cloud-runtime"
-  #   }
-  # }
