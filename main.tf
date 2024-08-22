64a11f591e2b542141d1ab499314cacd345c33e3
Liubov Smahliuk <liubov.smahliuk-contractor@procore.com>
2024-08-22 15:35:11 +0300

    test-run

--- main.tf
+++ main.tf
@@ -1,78 +1,29 @@
 provider "aws" {
-    region = "us-east-1"
-     default_tags {
-   tags = {
-     "pcor:system" = "Test"
-   }
- }
+  region = "us-east-1"
+  # default_tags {
+  #   tags = {
+  #     "pcor-system" = "Test"
+  #     "pcor-system" = "equipment"
+  #     "pcor-domain" = "cloud-runtime"
+  #   }
+  # }
 }
 
-resource "aws_s3_bucket" "this" {
-    tags = {
-      "Name" : "fancy"
-    }
-    force_destroy = true
+resource "aws_instance" "my_instance" {
+  ami           = "ami-08a0d1e16fc3f61ea"  # Example AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "Test pcor instance"
+    Environment = "Test"
+    "pcor-system" = "invalid-system"
+    "pcor-domain" = "invalid-domain"
+  }
 }
 
-
-resource "aws_s3_bucket" "this3" {
-    tags = {
-      Name        = "My this1"
-      Environment = "prod"
-      KK = "ss"
-      "pcor:system" = "equipment"
-      "pcor:domain" = "cloud-runtime"
-    }
-    timeouts {
-      create = "1s"
-    }
-
-}
-
-data "aws_s3_bucket" "invalid" {
-  bucket = aws_s3_bucket.this.bucket
-}
-
-# resource "aws_s3_bucket" "this4" {
-#     tags = {
-#       Name        = "My this1"
-#       Environment = "prod"
-#     }
-#     timeouts {
-#       create = "0s"
-#     }
-# }
-# resource "aws_instance" "this" {
-#     instance_type = "t2.micro"
-#     ami = "ami-0b0ea68c435eb488d"
-#     timeouts {
-#       create = "1s"
-#     }
-# }
-# output "bucket_this3" {
-#     value = aws_s3_bucket.this4.bucket
-    
-  
-# }
-
-# resource "aws_s3_bucket" "already_existing_bucket" {
-#     bucket = "terraform-state-aekoow9loo7voh4on5p"
-#     tags = {
-#       Name        = "My bucket"
-#     }
-#     # depends_on = [ aws_s3_bucket.this, aws_s3_bucket.this1, aws_s3_bucket.this2, aws_s3_bucket.this3, aws_s3_bucket.this4 ]
-#     # depends_on = [ null_resource.sleep ]
-# }
-
-# resource "null_resource" "sleep" {
-#   provisioner "local-exec" {
-#     command     = "sleep 60"
-#     interpreter = ["bash", "-c"]
-
+# data "aws_instance" "filtered_instance" {
+#   filter {
+#     name   = "tag:Environment"
+#     values = ["Development"]
 #   }
-# }
-# resource "aws_ssm_parameter" "dependant_resource" {
-#     name = "/dependant/bucket_id"
-#     value = aws_s3_bucket.already_exeisting_bucket.id
-#     type = "String"
 # }
